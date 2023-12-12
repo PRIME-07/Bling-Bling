0848be7fdab74c4ea788db1f09649a353220444a
PRIME-07 <anujm0707@gmail.com>
2023-12-12 13:22:58 +0530

    added number of lines function and main function

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+# max number of lines available to place bets
+MAX_LINES = 3
+
+
+# takes input for deposit amount
 def deposit():
     while True:
         amount = input("Enter amount to deposit: ")
@@ -11,4 +16,28 @@ def deposit():
             print("Enter valid amount")
     return amount
 
-deposit()
+
+def get_number_of_lines():
+    while True:
+        lines = input("Enter the number of lines to place bet on (1-" + str(MAX_LINES) + "): ")
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
+                break
+            else:
+                print("Enter valid number of lines.")
+        else:
+            print("Please pick number of lines.")
+    return lines
+
+# main function
+def main():
+    balance = deposit()
+    lines = get_number_of_lines()
+    print("Deposit amount = Rs.", end="")
+    print(balance)
+    print("Number of lines = ", end="")
+    print(lines)
+
+main()
+
