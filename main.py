2fa0238d31e66b7664b8556095c86e6bad2da286
PRIME-07 <anujm0707@gmail.com>
2023-12-12 20:25:45 +0530

    now the slot machine can show the generated symbols

--- main.py
+++ main.py
@@ -37,6 +37,18 @@ def get_slot_machine_spin(rows, cols, symbols):
     return columns
 
 
+def print_slot_machine(columns):
+    for row in range (len(columns[0])):
+        for i, column in enumerate(columns):
+            if i != len(columns) - 1:
+                print(column[row], "|")
+            else:
+                print(column[row])
+
+
+
+
+
 # takes input for deposit amount
 def deposit():
     while True:
